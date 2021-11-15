2b72b4dda21336c2588662cd4ff6a8091940c9d6
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-11-15 23:07:02 +0100

    setup files and add object array

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const app = new Vue ({
+    el: '#app',
+    data : {
+        todos = [
+            {
+                text: 'terminare Web App',
+                completed: false,
+            },
+            {
+                text: 'fare la spesa',
+                completed: false,
+            },
+            {
+                text: 'fare il bucato',
+                completed: true,
+            },
+        ]
+    }
+});
