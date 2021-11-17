3b126b84695c28a855c77dcfa802395f52b05c0b
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-11-17 17:48:15 +0100

    text decoration

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// Partire dalla definizione di un array con 3 oggetti ( i todo).
+// Ogni oggetto todo avrà le seguenti proprietà:
+// - text, una stringa che indica il testo del todo
+// - completed, un booleano (true/false) che indica se il todo è stato completato oppure no
+// MILESTONE 1
+// Stampare all’interno di una lista, un item per ogni todo.
+// Se la proprietà completed è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco di ogni todo una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+
 const app = new Vue ({
     el: '#app',
     data : {
@@ -16,4 +29,4 @@ const app = new Vue ({
             },
         ]
     }
-});
+}); 
